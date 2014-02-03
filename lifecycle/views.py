ef16aa2bc3909863fa7167e6f0457b509eec9c6c
dreampocket <dreampocketit@gmail.com>
2014-02-03 21:33:34 +0800

    db

--- lifecycle/views.py
+++ lifecycle/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from lifecycle.models import LifeCycleCurve as lc
 
 # Create your views here.
+def index(request):
+	date_number = lc.objects.all()
+	context = {'date_number': date_number}
+	return render(request, "lifecycle/index.html", context)
