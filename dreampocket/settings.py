9d0c8bb0b811cb6d35cf7e6124ef0a5a764524c6
C.C.T <changcheng.tu@gmail.com>
2014-03-16 11:02:28 +0800

    new homepage

--- dreampocket/settings.py
+++ dreampocket/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'lifecycle',
-	'warehouse',
+#	'lifecycle',
+#	'warehouse',
+    'home',
 )
 
 MIDDLEWARE_CLASSES = (
--- dreampocket/urls.py
+++ dreampocket/urls.py
@@ -3,11 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dreampocket.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-	url(r'^lifecycle/', include('lifecycle.urls')),
+urlpatterns = patterns('', 
+	url(r'^home/', include('home.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
